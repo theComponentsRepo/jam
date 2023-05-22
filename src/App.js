2144353c6249d1ed59d5618f1469630e9c17be4a
danielkellydev <danielkellydev@gmail.com>
2023-05-22 11:21:25 +1000

    created Search component, created Search page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import { Navigation } from './components/pageNavigation';
+import { Navigation } from './components/PageNavigation';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/music/search" element={<h1>searchpage</h1>} />
+        <Route path="/music/search" element={<h1><SearchPage /></h1>} />
         <Route path="/music/album/:id" element={<h1>album playlist page</h1>} />
         <Route path="/music/favourite" element={<h1>favourite page</h1>}/>
       </Routes>
