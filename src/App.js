ed18dc1bb693627e53512b1d8db6fc28cb04af50
Liam-M-Dev <liammassey21@gmail.com>
2023-05-23 15:54:46 +1000

    Create Audio Player function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import { Navigation } from './components/PageNavigation';
 import SearchPage from './pages/SearchPage';
 import Album from './components/Album';
+import AudioPlayer from './components/Player';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/music/search" element={<SearchPage />} />
         <Route path="/music/album/:id" element={<Album />} />
         <Route path="/music/favourite" element={<h1>favourite page</h1>}/>
+        <Route path='/audioplayer' element={<AudioPlayer />} />
       </Routes>
       
     </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,10 +5,59 @@ import { BiSkipNext, BiSkipPrevious } from "react-icons";
 import { IconContext } from "react-icons";
 
 
-export default function AudioPlayer(props){
-    
+export default function AudioPlayer(){
+
+    const [localTrackData, setLocalTrackData] = useState({});
+
+    const url = 'https://deezerdevs-deezer.p.rapidapi.com/track/3135556';
+    console.log(url)
+    const options = {
+        method: 'GET',
+        headers: {
+        'X-RapidAPI-Key': '577990ecfemshcaf230c6fa2818dp1d2fa8jsn711963dcaf86',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+        }
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(url, options);
+            const data = await response.json();
+            console.log(data)
+            setLocalTrackData(data)
+          } catch (error) {
+            console.error(error);
+          }
+      }
+      fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const songUrl = localTrackData.preview
     const [isPlaying, setIsPlaying] = useState(false);
 
-    const [play, { pause, duration, sound }] = useSound(props)
+    const [play, { pause, duration, sound }] = useSound(songUrl)
 
+    const playButton = () => {
+        if (isPlaying) {
+            pause();
+            setIsPlaying(false);
+        } else {
+            play();
+            setIsPlaying(true);
+        }
+    }
+
+    
+    console.log(localTrackData)
+
+    return(
+        <div>
+            <h1>Audio</h1>
+            <button onClick={playButton}>Play</button>
+        </div>
+    )
 }
+
