eccb84312d3d012579f06be5b120da029062f226
Liam-M-Dev <liammassey21@gmail.com>
2023-05-23 19:24:26 +1000

    Removed useSound hook, creating audio player using audio render and conditional rendering

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,8 +7,10 @@ import useSound from "use-sound";
 
 export default function AudioPlayer(){
 
+    // Create local track data variable to store the track's data from api
     const [localTrackData, setLocalTrackData] = useState({});
 
+    // Jie's code for fetching data from deezer API
     const url = 'https://deezerdevs-deezer.p.rapidapi.com/track/3135556';
     console.log(url)
     const options = {
@@ -35,67 +37,15 @@ export default function AudioPlayer(){
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    // const songUrl = localTrackData.preview;
+    const songUrl = localTrackData.preview
+
+    // Create variable isPlaying to track if song is currently playing or not
     const [isPlaying, setIsPlaying] = useState(false);
     // console.log(isPlaying)
 
-    const [play, {pause, duration, sound}] = useSound("https://cdns-preview-d.dzcdn.net/stream/c-deda7fa9316d9e9e880d2c6207e92260-10.mp3");
-    
-
-    const [time, setTime] = useState({
-        min: "",
-        sec: ""
-    })
-
-    const [currTime, setCurrTime] = useState({
-    min: "",
-    sec: ""
-    })
-
-    const [seconds, setSeconds] = useState();
-
-    useEffect(() => {
-        if(duration){
-            const sec = duration/1000;
-            const min = Math.floor(sec / 60);
-            const secRemain = Math.floor(sec % 60);
-            setTime({
-                min: min,
-                sec: secRemain
-            });
-        }
-        
-    }, [isPlaying]);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (sound) {
-                setSeconds(sound.seek([]));
-                const min = Math.floor(sound.seek([]) / 60);
-                const sec = Math.floor(sound.seek([]) % 60);
-                setCurrTime({
-                    min,
-                    sec,
-                });
-
-            }
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [sound])
-
-
-
-    const playButton = () => {
-        if (!isPlaying) {
-            pause();
-            setIsPlaying(true);
-            console.log(isPlaying)
-        } else {
-            play();
-            setIsPlaying(false);
-            console.log(isPlaying)
-        }
-    }
+    const togglePlay = () => {
+        setIsPlaying(!isPlaying)
+    };
 
 
 
@@ -104,26 +54,9 @@ export default function AudioPlayer(){
             <h1>Audio</h1>
             <h3>{localTrackData.title}</h3>
             {/* <h3>{localTrackData.artist.name}</h3> */}
-            <button onClick={playButton}>Play</button>
-            <div>
-                <p>
-                    {currTime.min}:{currTime.sec}
-                </p>
-                <p>
-                    {time.min}:{time.sec}
-                </p>
-                <input 
-                    type="range"
-                    min="0"
-                    max={duration / 1000}
-                    default="0"
-                    value={seconds}
-                    className="timeline"
-                    onChange={(event) => {
-                        sound.seek([event.target.value]);
-                    }}
-                />
-            </div>
+            <button onClick={togglePlay}>{isPlaying ? "pause":"play"}</button>
+            {isPlaying && <audio src={songUrl} autoPlay />}
+            
         </div>
     )
 }
