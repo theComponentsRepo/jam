0b8545d2aec104ccdac2dab47a1be480cb8a4a99
danielkellydev <danielkellydev@gmail.com>
2023-05-25 09:53:26 +1000

    removed unused useRef import

--- src/pages/AlbumPage.jsx
+++ src/pages/AlbumPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import AudioPlayer from "../components/AudioPlayer";
 import Tracks from "../components/Track";
