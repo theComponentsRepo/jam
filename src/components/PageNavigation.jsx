c03a51c14f65f80e23d753760e4de7211987b7b9
Jiezhoue <13809@coderacademy.edu.au>
2023-05-25 16:20:38 +1000

    create logo

--- src/components/PageNavigation.jsx
+++ src/components/PageNavigation.jsx
@@ -4,8 +4,10 @@ import { AiFillHome, AiOutlineSearch, AiFillHeart } from 'react-icons/ai';
 
 export const Navigation = () => {
   return (
-    <nav className='flex justify-center content-center p-2 bg-slate-100 sticky top-0 left-0 right-0 mb-2 shadow-lg'>
+    <nav className='flex justify-between content-center p-2 bg-slate-100 sticky top-0 left-0 right-0 mb-2 shadow-lg'>
+      <h5 className="font-['Caveat'] text-left text-3xl pl-3">Jam</h5>
       <ul className='nav-list'>
+
         <li>
           <NavLink exact to='/' activeClassName='active'>
             <AiFillHome className='nav-icon' />
