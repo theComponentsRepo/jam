4a3a2f4f0ffad294b50515d85df4cffa44fe76ec
Jiezhoue <13809@coderacademy.edu.au>
2023-05-26 13:53:37 +1000

    small css changes

--- src/components/AlbumLarge.jsx
+++ src/components/AlbumLarge.jsx
@@ -26,15 +26,15 @@ export default function AlbumLarge(props) {
       { data && 
       <div className="album-large-container p-4 flex bg-cyan-600 text-neutral-100 xs:flex-col xs:items-center xs:gap-2">
 
-        <div className="album-large-img pr-4 drop-shadow-xl xs:pr-0 xs:w-64 sm:w-56 md:w-72 lg:w-72	">
+        <div className="album-large-img px-3 drop-shadow-xl xs:px-0 xs:w-64 sm:w-56 md:w-72 lg:w-72	">
           <img src={data.cover_big} alt="" />
         </div>
         <div className="album-large-content text-left	flex flex-col justify-between">
           <div className="album-title">
             <p className="drop-shadow-xl xs:text-xs ">Album</p>
-            <h1 className=" font-extrabold	drop-shadow-xl  xs:text-md xs:leading-tight sm:text-2xl md:text-5xl lg:text-6xl">{data.title}</h1>
+            <h1 className=" font-extrabold	drop-shadow-xl  xs:text-md xs:leading-tight sm:text-2xl sm:leading-none md:text-5xl lg:text-6xl">{data.title}</h1>
           </div>
-          <ul className="font-light list-disc flex xs:text-xs xs:leading-tight sm:flex-col sm:list-none sm:gap-0 sm:text-xs gap-5">
+          <ul className="font-light list-disc flex xs:text-xs xs:leading-tight sm:leading-none sm:flex-col sm:list-none sm:gap-0 sm:text-xs gap-5">
           
             <li className="sm:font-bold md:list-none lg:list-none xs:list-none"> {data.artist}</li>
             <li> {extractYear(data.release_date)}</li> 
