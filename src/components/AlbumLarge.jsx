d7303330588d631a38fbe11c2e6090375bb5b8ff
Jiezhoue <13809@coderacademy.edu.au>
2023-05-26 13:39:59 +1000

    fix the heart shape overlap

--- src/components/AlbumLarge.jsx
+++ src/components/AlbumLarge.jsx
@@ -16,23 +16,28 @@ export default function AlbumLarge(props) {
     return result;
   }
 
+  const extractYear = (dateString) => {
+    const date = new Date(dateString);
+    return date.getFullYear();
+  };
+
   return (
     <div>
       { data && 
       <div className="album-large-container p-4 flex bg-cyan-600 text-neutral-100 xs:flex-col xs:items-center xs:gap-2">
 
-        <div className="album-large-img pr-4 drop-shadow-xl xs:w-64 sm:w-56 md:w-72 lg:w-72">
+        <div className="album-large-img pr-4 drop-shadow-xl xs:pr-0 xs:w-64 sm:w-56 md:w-72 lg:w-72	">
           <img src={data.cover_big} alt="" />
         </div>
         <div className="album-large-content text-left	flex flex-col justify-between">
           <div className="album-title">
             <p className="drop-shadow-xl xs:text-xs ">Album</p>
-            <h1 className=" font-extrabold	drop-shadow-xl  xs:text-md xs:leading-tight sm:text-2xl md:text-4xl lg:text-5xl">{data.title}</h1>
+            <h1 className=" font-extrabold	drop-shadow-xl  xs:text-md xs:leading-tight sm:text-2xl md:text-5xl lg:text-6xl">{data.title}</h1>
           </div>
           <ul className="font-light list-disc flex xs:text-xs xs:leading-tight sm:flex-col sm:list-none sm:gap-0 sm:text-xs gap-5">
           
             <li className="sm:font-bold md:list-none lg:list-none xs:list-none"> {data.artist}</li>
-            <li> {data.release_date}</li> 
+            <li> {extractYear(data.release_date)}</li> 
             <li> {convertSecondsToHoursAndMinutes(data.duration)}</li>  
             <li> {data.numOfSongs} Songs</li>
           </ul>
